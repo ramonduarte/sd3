56764eb026efd2dd07ac52eeb24bdaa3ce436b38
ramonmelo <ramonduarte@poli.ufrj.br>
2018-06-02 17:59:42 -0300

    Sockets should close automatically in case of failure.

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ Ramon Melo (ramonduarte at poli.ufrj.br)
 import signal
 import logging
 from conf import *
+from manager import user_input_handler, signal_handler
+from classes import Process, ListenerThread, EmitterThread
 
 logging.basicConfig(filename=os.path.join(LOG_DIR, "sd3.log"), level=logging.DEBUG)
 LOG = logging.getLogger(__name__)
@@ -13,7 +15,18 @@ LOG = logging.getLogger(__name__)
 
 def main():
     """ Centralizing all computations on a single function."""
-    return 0
+    try:
+        process = Process("192.168.0.3")
+        print("Setting up emitter thread.")
+        process.emitter_thread.run()
+        print("Setting up listener thread.")
+        process.listener_thread.run()
+        print(process.pid)
+        return 0
+    except:
+        process.emitter_thread.socket.close()
+        process.listener_thread.socket.close()
+        return 1
 
 
 if __name__ == '__main__':
